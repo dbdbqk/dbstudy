a463300e026c6c0f130cefcabee1ce1c91894253
dbdbqk <dbdbqk2@naver.com>
2023-02-07 08:58:07 +0900

    2023-02-07

--- Day02_01_KEY(제품,주문).sql
+++ Day02_01_KEY(제품,주문).sql
@@ -18,7 +18,16 @@
         2) 삭제 규칙 : "반드시" 자식 테이블을 먼저 삭제한다.
 */
 
-
+/*
+    외래키 제약 조건의 옵션
+    1. ON DELETE CASCADE
+        1) 참조 중인 PARENT KEY가 삭제 되면 해당 PARENT KEY를 가진 행 전체를 함께 삭제 한다.
+        2) 예시) 회원 탈퇴 시 작성한 모든 게시글이 함께 삭제 됩니다.
+                 게시글 삭제 시 해당 게시글에 달린 모든 댓글이 함께 삭제됩니다.
+    2. ON DELETE SET NULL
+        1) 참조 중인 PARENT KEY가 삭제 되면 해당 PARENT KEY를 가진 칼럼 값만 NULL로 처리 한다.
+        2) 예시) 어떤 상품을 제거 하였으나 해당 상품의 주문 내역은 남아 있는 경우
+*/
 -- 테이블 삭제 테이블 삭제 규칙은 반드시 자식 테이블 먼저 삭제  팁은 만드는거 먼저 후 삭제를 반대방향으로 적어서 삭제
 DROP TABLE ORDER_TBL;
 DROP TABLE PRODUCT_TBL;
@@ -39,7 +48,7 @@ CREATE TABLE ORDER_TBL (
     PROD_NO NUMBER, -- PRODUCT_TBL에 PROD_NO를 참조 한다. / FK_테이블명_부모테이블명
     ORDER_DATE DATE,
     CONSTRAINT PK_ORDER  PRIMARY KEY(ORDER_NO),
-    CONSTRAINT FK_ORDER_PROD FOREIGN KEY(PROD_NO) REFERENCES PRODUCT_TBL(PROD_NO) --(CONSTRAINT) 제약조건 이름 주기 테이블명 (REFERENCES) 부모 연결
+    CONSTRAINT FK_ORDER_PROD FOREIGN KEY(PROD_NO) REFERENCES PRODUCT_TBL(PROD_NO) ON DELETE SET NULL -- ON DELETE CASCADE --(CONSTRAINT) 제약조건 이름 주기 테이블명 (REFERENCES) 부모 연결
 );
 
 /*
